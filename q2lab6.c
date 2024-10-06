28ec2ad08b965bdbd6e6bf38932d8901760b6e3c
Laiba-17A <laiba17javed@gmail.com>
2024-10-06 01:38:57 -0700

    Update q2lab6.c

--- q2lab6.c
+++ q2lab6.c
@@ -7,7 +7,8 @@ printf("enter num1(smaller num)");
 scanf("%d",&a);
 printf("enter num2(larger num)");
 scanf("%d",&b);
-
+if (a==1)
+a=a+1;
 for (i=a;i<=b;)
 {
 if (a%2!=0)
@@ -18,8 +19,6 @@ sum=sum+a;
 i=i+2;
 a=i;
 }
-
-
 printf("the sum is %d",sum);
 
 return 0;
