9a9c01c5b25b06aca3f1e8ae6fb21e79e2c0ea33
Laiba-17A <laiba17javed@gmail.com>
2024-10-06 02:13:48 -0700

    Update q3lab6.c

--- q3lab6.c
+++ q3lab6.c
@@ -2,20 +2,21 @@
 int main()
 {
 int a,i;
-printf("enter atm pin\n");
-scanf("%d",&a);
-for(i=1;i<3;i++)
+i=1;
+while(i<=3)
 {
-	if (a!=5678)
+    printf("enter atm pin\n");
+   scanf("%d",&a);
+    if (a!=5678)
 	{
-		printf("enter correct pin\n");
-		scanf("%d",&a);
+    i=i+1;
+    continue;
 	}
 	else
 	{
 		printf("correct pin");
 	return 0;
-}
+    }
 }
 	printf("your card is blocked");
 return 0;
